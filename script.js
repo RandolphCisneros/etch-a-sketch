27eed741474f9e2440c489f0b79af31561da81a2
Randolph Cisneros <cisnerosRandolph@gmail.com>
2019-09-04 20:59:03 -0400

    etch-a-sketch add square style, row

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let outerContainer = document.getElementById('outerContainer');
 
 const gridSize = 2;
+const gridSquareClassName = "gridSquare";
+
 
 initializeInnerContainers();
 
 function initializeInnerContainers() {
     for (let i = 0; i < gridSize; i++){
         for (let j = 0; j < gridSize; j++) {
+            let gridDiv = document.createElement('div');
+            gridDiv.className = gridSquareClassName;
             let currentId = i.toString() + j.toString();
+            gridDiv.id = currentId;
+            gridDiv.textContent = currentId;
+            outerContainer.appendChild(gridDiv);
         }
     }
 }
