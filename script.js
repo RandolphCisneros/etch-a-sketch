71b19137ea6ad6b87f7804a580f5ddad54c072ff
Randolph Cisneros <cisnerosRandolph@gmail.com>
2019-10-05 07:26:39 -0400

    etchasketch add row functionality

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 let outerContainer = document.getElementById('outerContainer');
 
-const gridSize = 2;
+const gridSize = 3;
 const gridSquareClassName = "gridSquare";
+const rowClassName = 'rowContainer';
 
 
 initializeInnerContainers();
 
 function initializeInnerContainers() {
     for (let i = 0; i < gridSize; i++){
+        let rowDiv = document.createElement('div');
+        rowDiv.className = rowClassName;
+        rowDiv.id = 'row' + i;
+
         for (let j = 0; j < gridSize; j++) {
             let gridDiv = document.createElement('div');
             gridDiv.className = gridSquareClassName;
             let currentId = i.toString() + j.toString();
             gridDiv.id = currentId;
             gridDiv.textContent = currentId;
-            outerContainer.appendChild(gridDiv);
+            rowDiv.appendChild(gridDiv);
         }
+        let lineBreak = document.createElement('br');
+        rowDiv.appendChild(lineBreak);
+        outerContainer.appendChild(rowDiv);
     }
 }
